670c1370629c70a97ed197c4df31d1bc3c70283a
okutabio03 <nefricko@hotmail.com>
2016-11-06 22:41:53 -0600

    EDIT clase Evor_Cipher.java Se le agregaron los metodos generaCadenaAdicion y metodoBinario.
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .idea/workspace.xml
    	modified:   src/evor/Evor_Cipher.java

--- src/evor/Evor_Cipher.java
+++ src/evor/Evor_Cipher.java
@@ -1,6 +1,38 @@
 package evor;
 
+import java.util.List;
+
 public class Evor_Cipher {
+
+    protected List<Long> generaCadenaAdicion(List<Long> lsListaCadenaAdicion, long lExponente) {
+        long lBits = 1;
+        do {
+            lsListaCadenaAdicion.add(lBits);
+        } while ((lBits *= 2) <= lExponente);
+        return lsListaCadenaAdicion;
+    }
+
+    protected List<Long> metodoBinario(List<Long> lsListaCadenaAdicion, long lExponente) {
+        int iIndiceInicial = 0;
+        int iIndiceMitad;
+        int iLongitudArreglo = lsListaCadenaAdicion.size() - 1;
+        Long lUltimoElemento;
+        Long lElementoCadena;
+        while (iIndiceInicial <= iLongitudArreglo) {
+            iIndiceMitad = (iIndiceInicial + iLongitudArreglo) / 2;
+            lUltimoElemento = lsListaCadenaAdicion.get(lsListaCadenaAdicion.size() - 1);
+            lElementoCadena = lUltimoElemento + lsListaCadenaAdicion.get(iIndiceMitad);
+            if (lElementoCadena <= lExponente) {
+                lsListaCadenaAdicion.add(lElementoCadena);
+                iIndiceInicial = 0;
+                iLongitudArreglo = lsListaCadenaAdicion.size() - 1;
+            } else {
+                iLongitudArreglo = iIndiceMitad - 1;
+            }
+        }
+        return lsListaCadenaAdicion;
+    }
+
     protected int euclides(int a, int b) {
         int r;
         while (b != 0) {
