5447522f2c54e5b2578630de36bdad79869931e0
okutabio03 <nefricko@hotmail.com>
2016-11-06 21:14:33 -0600

    EDIT clase Evor_Cipher.java Se le agregaron los metodos euclides y euclidesExtendido.
    Detalles:
    On branch master
    Changes to be committed:
    	modified:   .idea/workspace.xml
    	modified:   src/evor/Evor_Cipher.java

--- src/evor/Evor_Cipher.java
+++ src/evor/Evor_Cipher.java
@@ -1,5 +1,28 @@
 package evor;
 
 public class Evor_Cipher {
+    protected int euclides(int a, int b) {
+        int r;
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
 
+    protected int euclidesExtendido(int a, int b) {
+        int i = b, v = 0, d = 1;
+        while (a > 0) {
+            int t = i / a, x = a;
+            a = i % x;
+            i = x;
+            x = d;
+            d = v - t * x;
+            v = x;
+        }
+        v %= b;
+        if (v < 0) v = (v + b) % b;
+        return v;
+    }
 }
